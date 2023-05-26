a1c4f4a4988b79d21fd2ee678cbae66ab39ce20a
anashamimi <anashamimi2@gmail.com>
2023-05-26 17:16:12 +0200

    completed task

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -4,18 +4,21 @@
 <h1 class=" text-center">Characters Cads</h1>
 <div class="container">
     <div class="row">
-        <div class="card col-4">
+        @foreach ($table_character as $character)
+           <div class="card col-4">
             <div class="card-body">
-                <p class="fs-4 card-title">Card title</p>
-                <p class="fs-5 card-subtitle mb-2 text-muted">name: </p>
-                <p class="card-text"> lv.</p>
-                <p class="card-text">race: </p>
-                <p class="card-text">lifepoint: </p>
-                <p class="card-text"> <i class="fa-solid fa-dumbbell"></i></p>
-                <p class="card-text"> <i class="fa-solid fa-person-running-fast"></i></p>
+                <p class="fs-4 card-title">{{$character->name}}</p>
+                <p class="card-text">{{$character->level}} lv.</p>
+                <p class="card-text">race: {{$character->race}}</p>
+                <p class="card-text">lifepoint: {{$character->lifepoint}}</p>
+                <p class="card-text">{{$character->strenght}} <i class="fa-solid fa-dumbbell"></i></p>
+                <p class="card-text">{{$character->agility}} <i class="fa-solid fa-person-running-fast"></i></p>
+                <p class="card-text">weapon: {{$character->main_weapon}}</p>
 
             </div>
         </div>
+        @endforeach
+
     </div>
 
     <a class="text-center" href="{{ route('home') }}">Back to home</a>
